3b8afe1779cbd84c273418cbabe40e87127d30b2
Raymond Hawthorne <digital@wusf.org>
2022-02-28 21:13:19 -0500

    Add new pages via WordPress API

---
+++ src/pages/employment-opportunities.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import Loading from '../components/loading'
+import Error from '../components/error'
+import useFetch from '../utils/useFetch'
+import Layout from '../components/layout'
+
+export default function EmploymentOpportunities() {
+    const { loading, results, error } = useFetch('https://dev.wusf.digital/wp-json/wp/v2/pages/?include=696')
+  return (
+  <Layout>
+    <article className='content'>
+        { loading && <Loading /> }
+        { !loading && error ? <Error /> :
+        <div style={{width: '75%'}} dangerouslySetInnerHTML={{__html: results?.[0].content?.rendered}} />
+        }
+    </article>  
+  </Layout>
+  )
+}
