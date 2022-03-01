64b56a0409b8aae71629ba2d20f2c1ac9269b3f6
Raymond Hawthorne <digital@wusf.org>
2022-02-28 22:12:35 -0500

    Nav sidebar links now open in same window

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -21,19 +21,16 @@ export default function Navbar() {
                 <li>Programs</li>
                 <Link to='/ways-to-support'><li>Support</li></Link>
                 <a href='https://wusfnews.wusf.usf.edu/weather'
-                    target='_blank'
                     rel='noopener noreferrer'>
                     <li>Weather</li>
                 </a>
                 <a href='https://wusfnews.wusf.usf.edu/community-calendar'
-                    target='_blank'
                     rel='noopener noreferrer'>
                     <li>Events</li>
                 </a>
                 
                 <Link to='/about-us'><li>About Us</li></Link>
                 <a href='https://wusfnews.wusf.usf.edu/'
-                    target='_blank'
                     rel='noopener noreferrer'>
                     <li>News Home</li>
                 </a>
@@ -41,19 +38,20 @@ export default function Navbar() {
             <hr className='main-nav__divider' />
             <ul>
                 <a href='https://health.wusf.usf.edu/'
-                    target='_blank'
                     rel='noopener noreferrer'>
                     <li>Health News Florida</li>
                 </a>
                 <a href='https://wusfnews.wusf.usf.edu/show/florida-matters'
-                    target='_blank'
                     rel='noopener noreferrer'>
                     <li>Florida Matters</li>
                 </a>
                 <a href='https://wusfnews.wusf.usf.edu/term/university-beat'
-                    target='_blank'
                     rel='noopener noreferrer'>
                     <li>University Beat</li>
+                </a>
+                <a href='https://wusfnews.wusf.usf.edu/storycorps-tampa-bay'
+                    rel='noopener noreferrer'>
+                    <li>StoryCorps Tampa Bay</li>
                 </a>
                 
             </ul>
